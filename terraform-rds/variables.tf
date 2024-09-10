a803ea8a1876b1c863b0a51980e5dd3b3fb5b1bb
Nataliia Vorobets <157847383+NatVor@users.noreply.github.com>
2024-09-10 22:28:50 +0300

    Update variables.tf

--- terraform-rds/variables.tf
+++ terraform-rds/variables.tf
@@ -7,3 +7,20 @@ variable "db_password" {
   description = "RDS root user password"
   sensitive   = true
 }
+
+variable "public_subnet_cidr" {
+ description = "Public Subnet CIDR values"
+ default     = "10.0.1.0/24"
+}
+ 
+variable "private_subnet_cidrs" {
+ # type        = list(string)
+ description = "Private Subnet CIDR values"
+ default     = ["10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "azs" {
+ type        = list(string)
+ description = "Availability Zones"
+ default     = ["us-east-1a", "us-east-1b"]
+}
