1e15e834378d9ad5a8954434e3e9f4a258ae1dfa
Nataliia Vorobets <157847383+NatVor@users.noreply.github.com>
2024-09-27 12:57:53 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,191 +1,6 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.67.0"
-    }
-  }
-}
-
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "main"
-  }
-}
-
-# Internet Gateway for public subnets
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "igw"
-  }
-}
-
-# NAT Gateway for private subnets
-resource "aws_eip" "nat" {
-  domain = "vpc"
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public_us_east_1a.id
-
-  tags = {
-    Name = "nat"
-  }
-
-  depends_on = [aws_internet_gateway.igw]
-}
-
-resource "aws_route" "private_nat_route" {
-  route_table_id         = aws_route_table.private_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat.id
-}
-
-resource "aws_subnet" "private_us_east_1a" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.0.0/19"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    "Name"                            = "private-us-east-1a"
-    "kubernetes.io/role/internal-elb" = "1"
-    "kubernetes.io/cluster/teachua"      = "owned"
-  }
-}
-
-resource "aws_subnet" "private_us_east_1b" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.32.0/19"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    "Name"                            = "private-us-east-1b"
-    "kubernetes.io/role/internal-elb" = "1"
-    "kubernetes.io/cluster/teachua"      = "owned"
-  }
-}
-
-resource "aws_subnet" "public_us_east_1a" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.64.0/19"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    "Name"                       = "public-us-east-1a"
-    "kubernetes.io/role/elb"     = "1"
-    "kubernetes.io/cluster/teachua" = "owned"
-  }
-}
-
-resource "aws_subnet" "public_us_east_1b" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.96.0/19"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
-
-  tags = {
-    "Name"                       = "public-us-east-1b"
-    "kubernetes.io/role/elb"     = "1"
-    "kubernetes.io/cluster/teachua" = "owned"
-  }
-}
-
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-
-  tags = {
-    Name = "private"
-  }
-}
-
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = aws_vpc.main.cidr_block
-    gateway_id = "local"
-  }
-
-  tags = {
-    Name = "Private Route Table"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "public"
-  }
-}
-
-resource "aws_route_table_association" "private_us_east_1a" {
-  subnet_id      = aws_subnet.private_us_east_1a.id
-  route_table_id = aws_route_table.private.id
-}
-
-resource "aws_route_table_association" "private_us_east_1b" {
-  subnet_id      = aws_subnet.private_us_east_1b.id
-  route_table_id = aws_route_table.private.id
-}
-
-resource "aws_route_table_association" "public_us_east_1a" {
-  subnet_id      = aws_subnet.public_us_east_1a.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "public_us_east_1b" {
-  subnet_id      = aws_subnet.public_us_east_1b.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_iam_role" "teachua" {
-  name = "eks-cluster-teachua"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
-}
-
-resource "aws_iam_role_policy_attachment" "teachua_amazon_eks_cluster_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.teachua.name
-}
-
 resource "aws_eks_cluster" "teachua" {
   name     = "teachua"
-  version  = "1.24"
+  version  = "1.25"
   role_arn = aws_iam_role.teachua.arn
 
   vpc_config {
@@ -200,42 +15,6 @@ resource "aws_eks_cluster" "teachua" {
   }
 
   depends_on = [aws_iam_role_policy_attachment.teachua_amazon_eks_cluster_policy]
-}
-
-resource "aws_iam_role" "nodes" {
-  name = "eks-node-group-nodes"
-
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "nodes_amazon_eks_worker_node_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.nodes.name
-}
-
-resource "aws_iam_role_policy_attachment" "nodes_amazon_eks_cni_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.nodes.name
-}
-
-resource "aws_iam_role_policy_attachment" "nodes_amazon_ec2_container_registry_read_only" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.nodes.name
-}
-
-# Optional: only for "SSH" to EKS nodes.
-resource "aws_iam_role_policy_attachment" "amazon_ssm_managed_instance_core" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-  role       = aws_iam_role.nodes.name
 }
 
 resource "aws_eks_node_group" "private_nodes" {
@@ -253,7 +32,7 @@ resource "aws_eks_node_group" "private_nodes" {
 
   scaling_config {
     desired_size = 1
-    max_size     =2
+    max_size     = 2
     min_size     = 0
   }
 
@@ -271,26 +50,6 @@ resource "aws_eks_node_group" "private_nodes" {
     aws_iam_role_policy_attachment.nodes_amazon_ec2_container_registry_read_only,
   ]
 }
-
-resource "aws_iam_role_policy" "nodes_secrets_manager_access" {
-  name = "nodes-secrets-manager-access"
-  role = aws_iam_role.nodes.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "secretsmanager:GetSecretValue",
-          "secretsmanager:DescribeSecret"
-        ]
-        Resource = "arn:aws:secretsmanager:us-east-1:771919296983:secret:teachua-rds-cred"
-      }
-    ]
-  })
-}
-
 
 data "tls_certificate" "eks" {
   url = aws_eks_cluster.teachua.identity[0].oidc[0].issuer
