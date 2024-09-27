3eb9df344e3778f477075b33592c1e471c7b0d9d
Nataliia Vorobets <157847383+NatVor@users.noreply.github.com>
2024-09-27 13:00:27 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,43 @@
 variable "region" {
-  default = "us-east-1"
-}
-
-variable "db_password" {
-  description = "Password for the RDS database"
+  description = "The AWS region to deploy resources."
   type        = string
+  default     = "us-east-1"
 }
 
-# variable "db_name" {
-#  description = "Name of the RDS database"
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC."
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for the private subnets."
+  type        = list(string)
+  default     = ["10.0.0.0/19", "10.0.32.0/19"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for the public subnets."
+  type        = list(string)
+  default     = ["10.0.64.0/19", "10.0.96.0/19"]
+}
+
+variable "availability_zones" {
+  description = "List of availability zones to use."
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "tags" {
+  description = "Tags for the resources."
+  type        = map(string)
+  default = {
+    Name = "main"
+  }
+}
+
+# variable "db_password" {
+#  description = "Password for the RDS database"
 #  type        = string
-# }
+#}
+
