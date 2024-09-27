4ee2647ccb2f761f4a5dba8d4521cbcbc8ce822a
Nataliia Vorobets <157847383+NatVor@users.noreply.github.com>
2024-09-27 13:06:34 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,21 +5,3 @@ output "eks_cluster_endpoint" {
 output "rds_instance_endpoint" {
   value = aws_db_instance.teachua.endpoint
 }
-
-output "rds_hostname" {
-  description = "RDS instance hostname"
-  value       = aws_db_instance.teachua.address
-  sensitive   = true
-}
-
-output "rds_port" {
-  description = "RDS instance port"
-  value       = aws_db_instance.teachua.port
-  sensitive   = true
-}
-
-output "rds_username" {
-  description = "RDS instance root username"
-  value       = aws_db_instance.teachua.username
-  sensitive   = true
-}
