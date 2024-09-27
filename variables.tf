260668488b60db1b7b64daa9be3fd30a3a986104
Nataliia Vorobets <157847383+NatVor@users.noreply.github.com>
2024-09-27 13:03:07 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -36,6 +36,18 @@ variable "tags" {
   }
 }
 
+variable "eks_cluster_name" {
+  description = "The name of the EKS cluster."
+  type        = string
+  default     = "teachua"
+}
+
+variable "eks_version" {
+  description = "The Kubernetes version for the EKS cluster."
+  type        = string
+  default     = "1.25"
+}
+
 # variable "db_password" {
 #  description = "Password for the RDS database"
 #  type        = string
