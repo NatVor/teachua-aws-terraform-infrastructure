933b9903bd2b0f919adbd11887069e5c1330e9e5
Nataliia Vorobets <157847383+NatVor@users.noreply.github.com>
2024-09-27 10:18:51 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,13 @@
+variable "region" {
+  default = "us-east-1"
+}
 
+variable "db_password" {
+  description = "Password for the RDS database"
+  type        = string
+}
+
+# variable "db_name" {
+#  description = "Name of the RDS database"
+#  type        = string
+# }
